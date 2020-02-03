d0bc0f60512e4f9e7b3e3cabca4346b478b1d264
jfederico <jesus@123it.ca>
2020-02-03 14:26:29 -0500

    Added config.hosts and secrets.yml for produciton

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.hosts << ENV['URL_HOST'] || ''
 end
