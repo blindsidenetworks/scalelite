eb988d439f9e7f6ab42dad44a607eb5fa4eb1821
Calvin Walton <calvin.walton@blindsidenetworks.com>
2020-02-11 14:58:47 -0500

    Don't use multiple nested scopes in routes when one will do.

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  scope format: false, defaults: { format: 'xml' } do
-    scope 'bigbluebutton/api', as: 'bigbluebutton_api' do
-      get '/', to: 'bigbluebutton_api#index'
-      get 'isMeetingRunning', to: 'bigbluebutton_api#is_meeting_running', as: :is_meeting_running
-      get 'getMeetingInfo', to: 'bigbluebutton_api#get_meeting_info', as: :get_meeting_info
-      get 'getMeetings', to: 'bigbluebutton_api#get_meetings', as: :get_meetings
-      match 'create', to: 'bigbluebutton_api#create', via: [:get, :post]
-      get 'end', to: 'bigbluebutton_api#end'
-      get 'join', to: 'bigbluebutton_api#join'
-    end
+  scope 'bigbluebutton/api', as: 'bigbluebutton_api', format: false, defaults: { format: 'xml' } do
+    get '/', to: 'bigbluebutton_api#index'
+    get 'isMeetingRunning', to: 'bigbluebutton_api#is_meeting_running', as: :is_meeting_running
+    get 'getMeetingInfo', to: 'bigbluebutton_api#get_meeting_info', as: :get_meeting_info
+    get 'getMeetings', to: 'bigbluebutton_api#get_meetings', as: :get_meetings
+    match 'create', to: 'bigbluebutton_api#create', via: [:get, :post]
+    get 'end', to: 'bigbluebutton_api#end'
+    get 'join', to: 'bigbluebutton_api#join'
 
     match '*any', via: :all, to: 'errors#unsupported_request'
   end
