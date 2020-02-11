82c69dcee8161ede587612a8001ecf764b9b315c
Calvin Walton <calvin.walton@blindsidenetworks.com>
2020-02-11 14:57:40 -0500

    Enable ActiveRecord
    Uncommend the active_record load, and run rake app:update to pull in the
    ActiveRecord setup & configuration code.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,12 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  # Raise an error on page load if there are pending migrations.
+  config.active_record.migration_error = :page_load
+
+  # Highlight code that triggered database queries in logs.
+  config.active_record.verbose_query_logs = true
+
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
