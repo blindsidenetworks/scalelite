9d589e2e0c27ae0751217c0aadf0339b7731d62b
Jesus Federico <jesus@123it.ca>
2021-02-02 17:18:56 -0500

    Revert "Add recording constraints to scalelite-api (#408)" (#419)
    This reverts commit a1e15dd5655bcbb2d2f5eafa2280c9a52f29c0fd.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,14 +29,12 @@ Rails.application.configure do
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
-  
-  unless 'true'.casecmp?(ENV['DB_DISABLED'])
-    # Raise an error on page load if there are pending migrations.
-    config.active_record.migration_error = :page_load
 
-    # Highlight code that triggered database queries in logs.
-    config.active_record.verbose_query_logs = true
-  end
+  # Raise an error on page load if there are pending migrations.
+  config.active_record.migration_error = :page_load
+
+  # Highlight code that triggered database queries in logs.
+  config.active_record.verbose_query_logs = true
 
   Rails.logger = Logger.new(STDOUT)
 
