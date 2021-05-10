2936eea819ef8c61e53afaf406b7a3709f508abf
Samuel MARTIN MORO <faust64@gmail.com>
2021-05-10 13:40:31 +0200

    fix(recordings): not sure it works / to test

--- app/controllers/bigbluebutton_api_controller.rb
+++ app/controllers/bigbluebutton_api_controller.rb
@@ -279,6 +279,7 @@ class BigBlueButtonApiController < ApplicationController
     query = Recording.includes(playback_formats: [:thumbnails], metadata: [])
     query = query.with_recording_id_prefixes(params[:recordID].split(',')) if params[:recordID].present?
     query = query.where(meeting_id: params[:meetingID].split(',')) if params[:meetingID].present?
+    query = query.where(state: params[:state].split(',')) if params[:state].present?
 
     @recordings = query.order(starttime: :desc).all
     @url_prefix = "#{request.protocol}#{request.host}"
