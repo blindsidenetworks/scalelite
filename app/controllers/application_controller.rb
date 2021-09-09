7c8927bae0e57ac4adab44f6321212e3c845a234
amal <amal.rubyonrails@gmail.com>
2021-09-09 14:24:54 -0300

    Update bigbluebutton_api_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,32 +1,6 @@
 # frozen_string_literal: true
 
-class ApplicationController < ActionController::Metal
-  # These includes are based on ActionController::API but with the full rendering stack re-enabled
-  include AbstractController::Rendering
-
-  include ActionController::UrlFor
-  include ActionController::Redirecting
-  include ActionView::Layouts
-  include ActionController::Rendering
-  include ActionController::Renderers::All
-  include ActionController::ConditionalGet
-  include ActionController::ImplicitRender
-  include ActionController::StrongParameters
-
-  include ActionController::ForceSSL
-  include ActionController::DataStreaming
-  include ActionController::DefaultHeaders
-
-  include AbstractController::Callbacks
-  include ActionController::Rescue
-  include ActionController::Instrumentation
-  include ActionController::ParamsWrapper
-  include ActionController::RequestForgeryProtection
-
-  ActiveSupport.run_load_hooks(:action_controller_api, self)
-  ActiveSupport.run_load_hooks(:action_controller, self)
-  # Controller setup done, applications stuff is below
-
+class ApplicationController < ActionController::Base
   include BBBErrors
 
   rescue_from BBBError do |e|
