0012222cc99ec46a8c4e061a37e96c9adf14b4cd
amal <amal.rubyonrails@gmail.com>
2021-09-23 11:30:17 -0300

    SL-211:  Fix the issue recordings are shown to the integrations even if the file does not exist

--- lib/recording_importer.rb
+++ lib/recording_importer.rb
@@ -23,30 +23,32 @@ class RecordingImporter
           || raise(RecordingImporterError, "Failed to extract tar file: #{filename}")
 
         Dir.glob('*/*/metadata.xml').each do |metadata_xml|
-          logger.info("Found metadata file: #{metadata_xml}")
-          metadata = IO.read(metadata_xml)
-          recording, playback_format = Recording.create_from_metadata_xml(metadata)
-          next if recording.nil?
+          Recording.transaction do
+            logger.info("Found metadata file: #{metadata_xml}")
+            metadata = IO.read(metadata_xml)
+            recording, playback_format = Recording.create_from_metadata_xml(metadata)
+            next if recording.nil?
 
-          publish_format_dir = "#{Rails.configuration.x.recording_publish_dir}/#{playback_format.format}"
-          unpublish_format_dir = "#{Rails.configuration.x.recording_unpublish_dir}/#{playback_format.format}"
-          published_status = if recording.publish_updated
-                               recording.published
-                             elsif recording_import_unpublished
-                               false
-                             elsif !recording.published
-                               false
-                             else
-                               true
-                             end
-          format_dir = published_status ? publish_format_dir : unpublish_format_dir
-          protected = Rails.configuration.x.protected_recordings_enabled
-          recording.update!(published: published_status, protected: protected)
-          FileUtils.rm_rf("#{publish_format_dir}/#{recording.record_id}")
-          FileUtils.rm_rf("#{unpublish_format_dir}/#{recording.record_id}")
+            publish_format_dir = "#{Rails.configuration.x.recording_publish_dir}/#{playback_format.format}"
+            unpublish_format_dir = "#{Rails.configuration.x.recording_unpublish_dir}/#{playback_format.format}"
+            published_status = if recording.publish_updated
+                                 recording.published
+                               elsif recording_import_unpublished
+                                 false
+                               elsif !recording.published
+                                 false
+                               else
+                                 true
+                               end
+            format_dir = published_status ? publish_format_dir : unpublish_format_dir
+            protected = Rails.configuration.x.protected_recordings_enabled
+            recording.update!(published: published_status, protected: protected)
+            FileUtils.rm_rf("#{publish_format_dir}/#{recording.record_id}")
+            FileUtils.rm_rf("#{unpublish_format_dir}/#{recording.record_id}")
 
-          FileUtils.mkdir_p(format_dir)
-          FileUtils.mv("#{playback_format.format}/#{recording.record_id}", format_dir, force: true)
+            FileUtils.mkdir_p(format_dir)
+            FileUtils.mv("#{playback_format.format}/#{recording.record_id}", format_dir, force: true)
+          end
         end
         callback_data = CallbackData.find_by(meeting_id: recording.meeting_id)
         callback_data&.update(recording_id: recording.id)
