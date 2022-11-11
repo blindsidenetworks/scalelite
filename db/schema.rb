a617a8c98ad9c57b1a1fc96b120fbc0547206e26
Anatolii Borysov <borisano@gmail.com>
2022-11-11 12:45:41 -0700

    Implementing Multitenancy
    * Add Tenant model
    * Scope all Meetings and Recordings requests by Tenant
    * Test coverage
    * README.md updated to reflect new features
    * Add Rake tasks to manage Tenants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_25_200414) do
+ActiveRecord::Schema.define(version: 2023_01_18_215330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,13 @@ ActiveRecord::Schema.define(version: 2021_07_25_200414) do
     t.boolean "protected"
     t.index ["meeting_id"], name: "index_recordings_on_meeting_id"
     t.index ["record_id"], name: "index_recordings_on_record_id", unique: true
+  end
+
+  create_table "tenants", force: :cascade do |t|
+    t.string "name"
+    t.string "secrets"
+    t.index ["name"], name: "index_tenants_on_name", unique: true
+    t.index ["secrets"], name: "index_tenants_on_secrets", unique: true
   end
 
   create_table "thumbnails", force: :cascade do |t|
