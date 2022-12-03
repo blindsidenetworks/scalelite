52d0d86cf7b962930203fa2bc639e0dfd2d3ac7d
Anatolii Borysov <borisano@gmail.com>
2022-12-02 17:24:37 -0700

    Add method to return array of secrets

--- spec/models/tenant_spec.rb
+++ spec/models/tenant_spec.rb
@@ -36,4 +36,43 @@ RSpec.describe Tenant, redis: true do
       end
     end
   end
+
+  describe '.secrets_array' do
+    context 'with single secret' do
+      let(:secret) { Faker::Crypto.sha512 }
+      let(:tenant) { build_stubbed :tenant, secrets: secret}
+
+      it 'returns one element' do
+        expect(tenant.secrets_array.class).to eq Array
+        expect(tenant.secrets_array.size).to eq 1
+        expect(tenant.secrets_array).to include secrets
+      end
+    end
+
+    context 'with multiple secrets' do
+      let(:nb_of_secrets) { rand(3..10) }
+      let(:secrets_array) { [] }
+      let(:tenant) { build :tenant }
+
+      before do
+        nb_of_secrets.times do
+          hash = Faker::Crypto.sha512
+          secrets_array << hash
+        end
+        secrets_string = secrets_array.join(Tenant::SECRETS_SEPARATOR)
+
+        tenant.update(secrets: secrets_string)
+      end
+
+      it 'returns correct number of elements' do
+        expect(tenant.secrets_array.size).to eq nb_of_secrets
+      end
+
+      it 'contains all the elements' do
+        tenant.secrets_array.each do |tenant_secret|
+          expect(secrets_array).to include tenant_secret
+        end
+      end
+    end
+  end
 end
