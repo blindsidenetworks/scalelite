41a1038ed4b0b45694bf479dadc618c7035208e2
Anatolii Borysov <borisano@gmail.com>
2022-12-21 11:55:41 -0700

    Add Rspec helper files to the project

---
+++ spec/redis_helper.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module RSpec
+  module RedisHelper
+    # When this module is included into the rspec config,
+    # it will set up an around(:each) block to clear redis.
+    def self.included(rspec)
+      rspec.after(:each, redis: true) do |_example|
+        Redis.current.flushdb
+      end
+    end
+
+    CONFIG = { url: ENV["REDIS_URL"] || "redis://127.0.0.1:6379/1" }
+
+    def redis
+      @redis ||= ::Redis.connect(CONFIG)
+    end
+
+    def with_clean_redis
+      redis.flushall            # clean before run
+      begin
+        yield
+      ensure
+        redis.flushall          # clean up after run
+      end
+    end
+  end
+end
