2724f363c7870cb0d0e68798f94503f13e899b97
Mariam A <tomariam@gmail.com>
2023-06-08 11:11:44 -0400

    rollback last migration (#962)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_18_211340) do
+ActiveRecord::Schema.define(version: 2022_11_11_194252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
