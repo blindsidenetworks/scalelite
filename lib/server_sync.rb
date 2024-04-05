e93f3f71204713d9256caac79e49797a39975205
Jan Kessler <Ithanil@users.noreply.github.com>
2024-04-05 18:15:54 +0200

    Fixes & improvements for servers:yaml rake task (#1056)
    * use string keys over symbols in hash produced by servers:yaml task, such that YAML.safe_load consumes it correctly (with default arguments), and for better human write-/readability
    
    * fix 'enabled' value in servers:yaml output
    
    * fix server.enabled? in servers:yaml[verbose]
    
    * fix logic mistake which prevented state 'cordoned' to be outputted in output of servers:yaml[verbose]

--- lib/server_sync.rb
+++ lib/server_sync.rb
@@ -171,15 +171,15 @@ class ServerSync
   def self.dump(verbose)
     Server.all.to_h do |server|
       info = {
-        url: server.url,
-        secret: server.secret,
-        load_multiplier: server.load_multiplier.to_f || 1.0,
-        enabled: server.enabled,
+        "url" => server.url,
+        "secret" => server.secret,
+        "load_multiplier" => server.load_multiplier.to_f || 1.0,
+        "enabled" => server.enabled?,
       }
       if verbose
-        info[:state] = server.state.presence || server.enabled ? 'enabled' : 'disabled'
-        info[:load] = server.load.presence || -1.0
-        info[:online] = server.online
+        info["state"] = server.state.presence || (server.enabled? ? 'enabled' : 'disabled')
+        info["load"] = server.load.presence || -1.0
+        info["online"] = server.online
       end
       [server.id, info]
     end
