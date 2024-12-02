9a533634c76e10ba50b2a889481b4ec6644f9004
Jan Kessler <jakessle@uni-mainz.de>
2024-12-02 17:36:51 +0100

    update to Rails 7.2

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,11 +40,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove these lines if you're not using ActiveRecord or ActiveRecord fixtures
-  if respond_to?(:fixture_paths=) # Rails 7.2+
-    config.fixture_paths = [Rails.root.join('spec/fixtures')]
-  else
-    config.fixture_path = Rails.root.join('spec/fixtures')
-  end
+  config.fixture_paths << Rails.root.join('spec/fixtures')
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
